d003d00d1bcfbcff2d87895d77451502acbdcb69
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2019-04-06 12:25:06 -0300

    Create a state representation file

---
+++ state.js
@@ -0,0 +1,20 @@
+export const state = {
+  visibilityFilter: 'SHOW_ALL',
+  todos: [
+    {
+      id: 'id-1',
+      text: 'hey',
+      completed: true,
+    },
+    {
+      id: 'id-2',
+      text: 'ho',
+      completed: true,
+    },
+    {
+      id: 'id-3',
+      text: 'let\'s go',
+      completed: false,
+    },
+  ],
+};
