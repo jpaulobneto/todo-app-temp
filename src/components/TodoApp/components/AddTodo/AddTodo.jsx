58b9d76d5dcac8f99491d92838a87dc9e9bcc2d4
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2019-05-04 10:00:17 -0300

    Rewrite component using class to keep callback reference

--- src/components/TodoApp/components/AddTodo/AddTodo.jsx
+++ src/components/TodoApp/components/AddTodo/AddTodo.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const AddTodo = ({ onAddTodo }) => {
-  let input;
+export class AddTodo extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <>
-      <input
-        name="addTodo"
-        ref={(node) => {
-          input = node;
-        }}
-      />
-      <button
-        type="button"
-        onClick={() => {
-          onAddTodo(input.value);
-          input.value = '';
-        }}
-      >
-        Add
-      </button>
-    </>
-  );
-};
+    this.input = React.createRef();
+    this.handleOnAddTodo = this.handleOnAddTodo.bind(this);
+  }
+
+  handleOnAddTodo() {
+    const { onAddTodo } = this.props;
+    const { value } = this.input.current;
+
+    onAddTodo(value);
+
+    this.input.current.value = '';
+  }
+
+  render() {
+    return (
+      <>
+        <input
+          name="addTodo"
+          ref={this.input}
+        />
+        <button
+          type="button"
+          onClick={this.handleOnAddTodo}
+        >
+          Add
+        </button>
+      </>
+    );
+  }
+}
 
 AddTodo.propTypes = {
   onAddTodo: PropTypes.func.isRequired,
