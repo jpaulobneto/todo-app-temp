477737ab9b46307250e03424acd070e80c2de5b6
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2019-05-04 11:19:11 -0300

    Filter todos by currentFilter

--- src/containers/TodoList/TodoList.jsx
+++ src/containers/TodoList/TodoList.jsx
@@ -2,7 +2,20 @@ import { connect } from 'react-redux';
 import { TodoList as PresentationalComponent } from '../../components/TodoApp/components/TodoList/TodoList';
 import { toggleTodo } from '../../actions/actionCreators';
 
-const mapStateToProps = state => state;
+const getVisibleTodos = (todos, filter) => todos.filter((todo) => {
+  switch (filter) {
+    case 'SHOW_ACTIVE':
+      return todo.completed === false;
+    case 'SHOW_COMPLETED':
+      return todo.completed === true;
+    default:
+      return true;
+  }
+});
+
+const mapStateToProps = state => ({
+  todos: getVisibleTodos(state.todos, state.visibilityFilter),
+});
 const mapDispatchToProps = dispatch => ({
   onTodoClick: id => dispatch(toggleTodo(id)),
 });
